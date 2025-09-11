3d316b0b090021de9e4fd006c6c2a58a6804278a
Tyler Schnoor <tyler.t.schnoor@gmail.com>
2025-09-11 11:15:21 -0700

    fixed things to have a minimal wave reflection model

---
+++ matlab/flowsource.m
@@ -0,0 +1,105 @@
+function ug = flowsource(source_params,Fsp,Fs);
+% Author: B. Story, 09.20.10
+% Function to calculate a glottal flow signal to be used as a source for
+% Waveguide.m; Based on Titze, Mapes, and Story, (1994). J. Acoust. Soc.
+% Am.
+%Inputs:
+% source_params is a two column matrix where col 1 is the Fo and col 2 is
+% the flow amplitude
+% Fsp = sampling frequency of the source params (e.g. 200 Hz)
+% Fs = audio sampling frequency (e.g. 44100 Hz)
+
+%Example function call: 
+% ug = flowsource(source_params,200,44100);
+
+%---Set up some constants and defaults
+
+    rho = 0.00114;
+   
+    
+    Qs = 3;
+    Qo = .6;
+    
+    Uac = 0;
+    dt = 1/Fs;
+    
+%---- Set up some numbers for my interpolator - you probably won't need to do this
+
+    Nfrm = length(source_params);
+    N = floor((Nfrm/Fsp)*Fs);
+    Nwind = round(Fs/Fsp)
+    ncnt = 0;
+    frmcnt = 1;
+  
+ %---- Allocate memory for the output vector
+ 
+    ug = zeros(N,1);
+    
+ %---- Assign some initial params before the time loop begins   
+    freqp = source_params(1,1);
+    theta = 0;
+    gama = 64.*( Qs - 1.0);
+	beta = 	Qo + 1.0;
+    
+    
+    for n=1:N-Nwind
+        
+    %------This is all interpolation (Siddharth: you can use the same interpolator you are using 
+    % for the areas and lengths 
+        
+    if(ncnt < Nwind)
+    
+        A = source_params(frmcnt,1);
+	    B = source_params(frmcnt+1,1);
+	    freq = (B-A)*ncnt/Nwind + A;
+        
+        
+        A = source_params(frmcnt,2);
+	    B = source_params(frmcnt+1,2);
+	    ampl = (B-A)*ncnt/Nwind + A;
+		
+        ncnt = ncnt + 1;
+    else
+        ncnt = 0;
+	    frmcnt = frmcnt + 1;
+    end
+    
+    %-------------End of interpolation - Begin signal generator----------------------------
+    
+    
+        plg=ampl*freq/7.5;	
+	    vo=sqrt(2*plg/rho);	
+        
+       
+        theta = theta+pi*(freq + freqp)*dt;
+        
+        if(theta > 2*pi) 
+            theta = theta - 2*pi;
+            freqp = freq;
+        end;
+        
+        if(theta < Qo*2*pi-.00001 && ampl > 0) 
+		    th = theta/(2.* Qo);
+            sinfun = sin(th)^beta;
+		    ga = ampl*sinfun/vo;
+		    up = max(0.,Uac);
+		    Uac = max(0.,ampl*sinfun);
+		    d = max(0.,gama*sinfun);
+		    hold = sqrt( (Uac * Uac)*(1.+d*d)+2.*d*up * Uac );
+		    Uac = max(0.,hold-d * Uac);  
+	    else 
+		    up = Uac;
+		    Uac = 0.0;
+		    ga = 0.0; 
+        end;
+        
+        ug(n) = Uac;
+      
+        
+        
+    end;
+    
+    
+ ug = ug(1:round(0.99*length(ug)));   
+    
+	
